0310e9830c5d6075f38d9294b70d641d735bd670
ankitmlesmagico <ankit.p@esmagico.in>
2024-04-10 11:37:33 +0530

    component change to molecule

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
+const axios = require('axios')
+const fs = require('fs-extra')
+const path = require('path')
 
 async function fetchComponentsList(owner, repo, branch = 'main') {
-   
-    const indexJsonUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/components/index.json`;
+  const indexJsonUrl = `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/src/molecules/index.json`
 
-    try {
-        const response = await axios.get(indexJsonUrl);
-        const components = response.data.components;
-        return components;
-    } catch (error) {
-        console.error('Failed to fetch components list:', error);
-        return [];
-    }
+  try {
+    const response = await axios.get(indexJsonUrl)
+    const components = response.data.molecules
+    return components
+  } catch (error) {
+    console.error('Failed to fetch components list:', error)
+    return []
+  }
 }
 
 async function fetchFilePaths(owner, repo, componentPath, branch = 'main') {
-    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${componentPath}?ref=${branch}`;
-    try {
-        const response = await axios.get(apiUrl);
-        return response.data
-            .filter(item => item.type === 'file')
-            .map(file => ({ name: file.name, download_url: file.download_url }));
-    } catch (error) {
-        console.error(`Failed to fetch file paths for ${componentPath}:`, error);
-        return [];
-    }
+  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${componentPath}?ref=${branch}`
+  try {
+    const response = await axios.get(apiUrl)
+    return response.data
+      .filter((item) => item.type === 'file')
+      .map((file) => ({ name: file.name, download_url: file.download_url }))
+  } catch (error) {
+    console.error(`Failed to fetch file paths for ${componentPath}:`, error)
+    return []
+  }
 }
 
-async function copyComponent(owner, repo, componentName, destination, branch = 'main') {
-    const componentFiles = await fetchFilePaths(owner, repo, `components/${componentName}`, branch);
+async function copyComponent(
+  owner,
+  repo,
+  componentName,
+  destination,
+  branch = 'main'
+) {
+  const componentFiles = await fetchFilePaths(
+    owner,
+    repo,
+    `molecules/${componentName}`,
+    branch
+  )
 
-    const destinationPath = path.join(destination, componentName);
-    await fs.ensureDir(destinationPath); // Ensure the destination directory exists
+  const destinationPath = path.join(destination, componentName)
+  await fs.ensureDir(destinationPath) // Ensure the destination directory exists
 
-    for (const file of componentFiles) {
-        const filePath = path.join(destinationPath, file.name);
-        console.log(`Copying ${file.name} to ${filePath}...`);
+  for (const file of componentFiles) {
+    const filePath = path.join(destinationPath, file.name)
+    console.log(`Copying ${file.name} to ${filePath}...`)
 
-        const response = await axios.get(file.download_url, { responseType: 'arraybuffer' });
-        await fs.writeFile(filePath, response.data);
-        console.log(`${file.name} copied successfully.`);
-    }
+    const response = await axios.get(file.download_url, {
+      responseType: 'arraybuffer',
+    })
+    await fs.writeFile(filePath, response.data)
+    console.log(`${file.name} copied successfully.`)
+  }
 }
 
-module.exports = { fetchComponentsList, copyComponent };
-
-
-
- 
+module.exports = { fetchComponentsList, copyComponent }
